1c55a1d553ecbe56170c69bc7e9857d6f11fc2fc
Yuukilari <larissaaguiar80@gmail.com>
2026-05-22 02:58:24 -0300

    Ajustando erro de locale.

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -1,18 +1,20 @@
-from flask import Blueprint, render_template, redirect, session, url_for
+from flask import Blueprint, render_template
 from auth import login_required
-import locale
 from datetime import datetime
 
 home_bp = Blueprint("home", __name__)
 
+def formatar_data_pt(data):
+    meses = [
+        "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+        "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+    ]
+    return f"{data.day} de {meses[data.month - 1]} de {data.year}"
+
+
 @home_bp.route("/home")
 @login_required
 def index_home():
-    locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
+    hoje = formatar_data_pt(datetime.now())
 
-    hoje = datetime.now().strftime("%d de %B de %Y")
-
-    return render_template("home/index.html", hoje = hoje)
-
-
-
+    return render_template("home/index.html", hoje=hoje)
